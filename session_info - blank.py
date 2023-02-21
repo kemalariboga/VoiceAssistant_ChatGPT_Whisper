b967e2309ff2767c3e6680190d0f5e87edbc99c4
kemal <kemalburak94@gmail.com>
2023-02-22 01:28:42 +0300

    updated session_info

--- session_info - blank.py
+++ session_info - blank.py
@@ -1,5 +1,6 @@
 # 1) Log-in --> https://chat.openai.com/chat
 # 2) https://chat.openai.com/api/auth/session --> Go to this website and copy everything, then paste it to the "session_info" variable like below.
+# 3) Save this file as "session_info.py"
 
 session_info = {'user': 'user info', 'expires': 'expiration date', 'accessToken': 'token info'}
 
